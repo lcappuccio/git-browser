a3059e76f9e00feccf02d969b20d388e556664b9
Leonardo Cappuccio <leonardo.cappuccio@appway.com>
2017-07-27 18:13:40 +0200

    log initialization

--- src/main/java/com/appway/gitbrowser/pojo/GitLogContainer.java
+++ src/main/java/com/appway/gitbrowser/pojo/GitLogContainer.java
@@ -3,6 +3,8 @@ package com.appway.gitbrowser.pojo;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
@@ -10,6 +12,7 @@ import java.util.List;
 
 public class GitLogContainer {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(GitLogContainer.class);
 	private final List<RevCommit> revCommitList;
 
 	@Autowired
@@ -19,7 +22,9 @@ public class GitLogContainer {
 		Iterable<RevCommit> refCollection = gitRepository.log().call();
 		for (RevCommit revCommit: refCollection) {
 			revCommitList.add(revCommit);
+			LOGGER.info("Added " + revCommit.getId().getName());
 		}
+		LOGGER.info("Added " + revCommitList.size() + " commits");
 	}
 
 	public List<RevCommit> getRevCommitList() {
