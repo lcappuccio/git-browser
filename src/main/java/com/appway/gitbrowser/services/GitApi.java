83284e11e1a706c898e087bd6336762c08821708
Leonardo Cappuccio <leonardo.cappuccio@appway.com>
2017-07-27 16:54:33 +0200

    decouple interface

--- src/main/java/com/appway/gitbrowser/services/GitApi.java
+++ src/main/java/com/appway/gitbrowser/services/GitApi.java
@@ -1,25 +1,26 @@
 package com.appway.gitbrowser.services;
 
+import com.appway.gitbrowser.model.Commit;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.revwalk.RevCommit;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface GitApi {
 
 	/**
 	 * Get parent of a given commit
 	 *
-	 * @param revCommit
+	 * @param commit
 	 * @return
 	 */
-	RevCommit getParentOf(RevCommit revCommit) throws IOException;
+	Commit getParentOf(Commit commit) throws IOException, GitAPIException;
 
 	/**
 	 * Return the full commit list
 	 *
 	 * @return
 	 */
-	Iterable<RevCommit> getAllCommits() throws IOException, GitAPIException;
+	List<Commit> getAllCommits() throws IOException, GitAPIException;
 
 }
