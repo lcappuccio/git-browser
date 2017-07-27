7719b30b516ee96ef50771831a8730636fa979f2
Leonardo Cappuccio <leonardo.cappuccio@appway.com>
2017-07-27 11:59:21 +0200

    add graph api bean

--- src/main/java/com/appway/gitbrowser/Application.java
+++ src/main/java/com/appway/gitbrowser/Application.java
@@ -1,14 +1,26 @@
 package com.appway.gitbrowser;
 
+import com.appway.gitbrowser.services.GraphApi;
+import com.appway.gitbrowser.services.GraphApiImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+
+	@Value("${database.folder}")
+	private String databaseFolder;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);
 
+	}
+
+	@Bean
+	public GraphApi databaseApi() {
+		return new GraphApiImpl(databaseFolder);
 	}
 }
